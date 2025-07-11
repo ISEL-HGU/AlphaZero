201baa92b78898d72b1c6998725b4faf39e7cdd5
Hahyun Baek <hahyun8587@gmail.com>
2025-07-11 16:05:03 +0900

    feat: change noise injection

--- alphazero/tree/visitor.py
+++ alphazero/tree/visitor.py
@@ -1,25 +1,22 @@
 """Node visitors and selectors."""
 
-from math import sqrt, log
-from tkinter import N
 import tensorflow as tf
 import tensorflow_probability as tfp
 
 from alphazero.alphazero import BaseAlphaZero
-from alphazero.components.agent import Agent
 from alphazero.nn.model import Model
 from alphazero.tree.node import Node
+from alphazero.utils.math import puct
+
 
 class Selector():
     """Base selector.
-    
-    Selector modifies prior probabilities of root node and selects child node. 
 
     Note:
-        Descendants of `Selector` should override `select()`,  
-        `modify_priors()`, and `reset()`. 
+        Descendants of `Selector` should override `select()` and `reset()`.  
+        They also should call `__init__` of `Selector` in the constructor. 
         
-    Attributes: 
+    Attributes:
         _min_q (float): Minimum state-action value of the search tree.
         _max_q (float): Maximum state-action value of the search tree.
     """
@@ -30,16 +27,22 @@ class Selector():
         self._min_q = float('-inf')
         self._max_q = float('inf')
     
-    def update_min_max_q(self, q: float) -> None:
-        """Update `min_q` or `max_q` with the given state-action value.  
+    def update_min_max_q(self, q: float) -> float:
+        """Update minimum state-action value or maximum state-action value  
+        of the search tree with given state-action value.  
 
         Args:
             q (float): The state-action value.
+            
+        Returns:
+            float: The state-action value.
         """
         if q < self._min_q:
             self._min_q = q
         elif q > self._max_q:
             self._max_q = q
+        
+        return q
     
     def select(self, node: Node) -> int:
         """Calculate action number of the given node's child to visit.  
@@ -53,109 +56,101 @@ class Selector():
         raise NotImplementedError(f'class {self.__class__.__name__} did not \
                                   implement select().')
     
-    def modify_priors(self, priors: tf.Tensor) -> tf.Tensor:
-        """Modify the given priors by adding noise.
-        
-        Args:
-            priors (tf.Tensor): Prior probabilities.
-
-        Returns:
-            tf.Tensor: Prior probabilities with noise.
-        """
-        raise NotImplementedError(f'class {self.__class__} did not override \
-                                  modify_priors().')
-    
     def reset(self) -> None:
         """Reset states of this instance.
         """
         raise NotImplementedError(f'class {self.__class__} did not override \
                                   reset().')
 
+
 class PUCTSelector(Selector):
     """Selector that modifies prior probabilities with Dirichlet noise and  
     selects child node with PUCT algorithm.
 
     Attributes:
-        _diric_param (float): Dirichlet parameter.
-        _diric_weight (float): Dirichlet noise weight.
+        _noise (tf.Tensor): Diriclet noise
+        _concentration (float): Concenctration parameter.
+        _weight (float): Noise weight.
     """
-    def __init__(self, diric_param: float, diric_weight: float):
+    def __init__(self, concentration: float, weight: float, n):
         """Initialize `PUCTSelector` instance.
 
         Args:
-            diric_param (float): Dirichlet parameter.
-            diric_weight (float): Dirichlet noise weight.
+            concentration (float): Concentration parameter of dirichlet  
+                distribution.
+            weight (float): Dirichlet noise weight.
         """
-        self._diric_param = diric_param
-        self._diric_weight = diric_weight
+        super(PUCTSelector, self).__init__()
+        
+        self._noise = None
+        self._concentration = concentration
+        self._weight = weight
 
     def select(self, node: Node) -> int:
-        """Calculate action number of given node's child to visit with PUCT  
+        """Calculate action number of the child node to visit with PUCT  
         algorithm.
         
-        The formula for caculating the action number is as following:  
+        The formula for caculating the action number is as following:
+        .. math::
+            a=\\underset{a}{\\mathrm{argmax}}\\Bigg[Q(s,a)+P(s,a)\\frac{ \\
+            \\sqrt{\\sum_{b}N(s,b)}}{1+N(s,a)}\\Bigg(c_1+\\log(\\frac{ \\
+            \\sum_{b}N(s,b)+c_2+1}{c_2}))],
         
-        TO DO:
-            Add PUCT formula to this docstring.
+        where :math:`\\sum_{b}N(s,b)` denotes visit count of the parent node.  
+        :math:`c_1=1.25` and :math:`c_2=19652` is used. The formula is  
+        referenced by Schrittwieser et al., 2020.
         
         Note: 
             This method overrides `select()` of `Selector`.
+        
+        See Also:
+            https://www.nature.com/articles/s41586-020-03051-4
         """
-        C1 = 1.25
-        C2 = 19652
+        stats = node.obtain_stats_children()
         
-        return tf.argmax(tf.constant(
-                [child.get_q() + child.get_p() 
-                               * sqrt(node.get_n()) 
-                               / (1 + child.get_n()) 
-                               * (C1 + log((node.get_n() + C2 + 1) / C2)) 
-                 for child in node.get_children()])).numpy()
+        if self._noise is None: 
+            self._noise = tfp.distributions.Dirichlet(
+                    tf.fill(stats['P'].shape, self._concentration)).sample()
         
-    def modify_priors(self, priors: tf.Tensor) -> tf.Tensor:
-        """Modify the given priors by adding Dirichlet noise.
+        if node.is_root():
+            p = (1 - self._weight) * stats['P'] + self._weight * self._noise 
+        else: 
+            p = stats['P']
         
-        Note:
-            This method overrides `modify_priors()` of `Selector`.        
-        """
-        #tfp.distributions.Dirichlet(
-        #        tf.fill(priors.shape.as_list(), self._diric_param)).sample    
-    
+        return tf.argmax(puct(p, stats['Q'], stats['N'], node.get_n()))
+        
     def reset(self) -> None:
-        """Reset states of this instance.
-        
-        This method does nothing since there are no states in this instance.
+        """Reset noise of this instance.
         
         Note:
-            Override `reset()` of `Selector`.
+            This method overrides `reset()` of `Selector`.
         """
-        pass 
-
+        self._noise = None
+  
+      
 class NodeVisitor():
-    """Base node visitor.  
-    
+    """Base node visitor.
+
     Node visitor conducts one simulation of mcts when traversing the tree.  
     
     Note:
-        Descendants of `NodeVisitor` should override `check_terminal()`.
+        Descendants of `NodeVisitor` should override `check_terminal()`. They  
+        also should call `__init__` of `NodeVisitor`.
 
     Attributes:
         _model (Model): Neural network model.
          _selector (Selector): Selector.
-        _discount_factor (float): Discount factor.
     """
     
-    def __init__(self, model: Model, selector: Selector, 
-                 discount_factor: float):
+    def __init__(self, model: Model, selector: Selector):
         """Initialize `NodeVisitor` instance.
 
         Args:
             model (Model): Neural network model.
             selector (Selector): Selector.
-            discount_factor (float): Discount factor.
         """
         self._model = model
-        self._selector = selector
-        self._discount_factor = discount_factor 
+        self._selector = selector 
         
     def pre_visit_internal(self, node: Node) -> int:
         """Get an action number of current internal node's child to visit.  
@@ -181,24 +176,19 @@ class NodeVisitor():
         return self._backup(node, v)
     
     def _backup(self, node: Node, v: float) -> float: 
-        """Update statistics of the given node.
+        """Update statistics of given node with given state value.
 
         Args:
-            node (Node): Node in which this instance is located.
-            v (float): State value of the current node's child.
+            node (Node): The node in which this instance is located.
+            v (float): The state value.
 
         Returns:
-            float: State value of the current node.
+            float: State value of the current node in the perspective of its  
+                parent.
         """
-        if node.is_root():
-            return node.update_stats(v)
-        else:
-            g = node.get_r() + self._discount_factor * v
-            
-            self._selector.update_min_max_q(g)
-            
-            return node.update_stats(g)
-       
+        return (-2 * self._is_multiagent + 1) \
+                * self._selector.update_min_max_q(node.update_stats(v))
+                
     def visit_leaf(self, node: Node) -> float:
         """Visit current leaf node.
 
@@ -208,52 +198,52 @@ class NodeVisitor():
         Returns:
             float: State value of the current leaf node.
         """
-        return self._backup(node, 
-                            0 if self.check_terminal(node) 
-                            else self._expand_and_eval(node))
-    
-    def check_terminal(self, node: Node) -> bool:
-        """Check whether the given node is terminal or not. 
-
-        Args:
-            node (Node): Node in which this instance is located.
-
-        Returns:
-            bool: `True` if the given node is terminal node, `False` otherwise.
-        """
-        raise NotImplementedError(f'class {self.__class__} did not override \
-                                  check_terminal().')
+        return self._backup(node, self._expand_and_eval(node))
     
     def _expand_and_eval(self, node: Node) -> float:
-        """Expand the given node and evaluate the state value of the given  
-        node's children.
+        """Expand given node and evaluate state transition.
 
         Args:
             node (Node): Leaf node in which this instance is located.
 
         Returns:
-            float: State value of the current leaf node's children.
+            float: State value of the state transition.
         """
+        if node.is_terminal():
+            return 0
+        
         if node.is_root():
             s = BaseAlphaZero.factory.create_state(*self._model.encode(node.get_o()))
             node.set_s(s)
-            
-            ps, v = self._model.estimate(s)
-            ps = self._selector.modify_priors(ps)
         else: 
             r, s = self._model.process(node.get_intern_s(), node.get_a())
             node.set_r(r)
             node.set_s(s)
             
-            if s is None or s.is_terminal():
+            if self.check_terminal(node):
                 return 0
-             
-            ps, v = self._model.estimate(s)
+            
+        P, v = self._model.estimate(s)
         
-        for i, p in enumerate(ps):
-            node.add(Node(s, BaseAlphaZero.factory.create_action(i), p))
+        for i, p in enumerate(P):
+            node.add_child(
+                    Node(s, BaseAlphaZero.factory.create_action(i), 
+                         p, node.get_discount_factor()))
         
         return (-2 * self._is_multiagent + 1) * v 
+    
+    def check_terminal(self, node: Node) -> bool:
+        """Check whether given node should be considered as terminal or not.
+
+        Args:
+            node (Node): The node in which this instance is located.
+
+        Returns:
+            bool: `True` if the node should be considered as terminal, `False`  
+                otherwise.
+        """
+        raise NotImplementedError(f'class {self.__class__} did not override \
+                                  check_terminal().')
     
     def reset_selector(self) -> None:
         """Reset `selector` of this instance.
@@ -266,3 +256,25 @@ class NodeVisitor():
     
     def set_model(self, model: Model) -> None:
         self._model = model        
+
+
+class BoundedVisitor(NodeVisitor):
+    """Node visitior that stops expanding when it visits terminal node.
+    """
+    
+    def __init__(self, model: Model, selector: Selector):
+        """Initialize `BoundedVisitior` instance.
+
+        Args:
+            model (Model): Neural network model.
+            selector (Selector): Selector.
+        """
+        super(BoundedVisitor, self).__init__(model, selector)
+    
+    def check_terminal(self, node: Node):
+        """Check whether given node is terminal or not.
+        
+        Note: 
+            This method overrides `check_terminal` of `NodeVisitor`.
+        """
+        return node.is_terminal()
