6b72600eb31bb0a8d9f589335dbc4346ad34213c
Hahyun Baek <hahyun8587@gmail.com>
2025-08-14 15:37:34 +0900

    fix: change reward to have discount factor

--- alphazero/mdp/reward.py
+++ alphazero/mdp/reward.py
@@ -9,15 +9,24 @@ class Reward():
     
     Attributes:
         _val (float): Reward value.
+        _discount_factor (float): Discount factor.
     """
     
-    def __init__(self, val: float):
+    def __init__(self, val: float, discount_factor):
         """Initialize `Reward` instance.
 
         Args:
             val (float): Reward value.
+            discount_factor (float): Discount factor.
         """
         self._val = val
+        self._discount_factor = discount_factor
+    
+    def get_val(self) -> float:
+        return self._val
+    
+    def get_discount_factor(self) -> float:
+        return self._discount_factor
     
     def __add__(self, other):
         return self._val + other
