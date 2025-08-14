dc9cdb18786bb2aedeebf495298bc8ff9970d54f
Hahyun Baek <hahyun8587@gmail.com>
2025-08-14 15:57:35 +0900

    fix: divide simulating

--- alphazero/components/simulator.py
+++ alphazero/components/simulator.py
@@ -1,24 +1,20 @@
 """Simulator interface."""
 
+from alphazero import mdp
 from alphazero.mdp.action import Action
-from alphazero.mdp.factory import MDPFactory
 from alphazero.mdp.reward import Reward
-from alphazero.mdp.state import State
+from alphazero.mdp.state import Observation, State
+
 
 class Simulator():
     """Simulator that outputs reward and next state.
 
     Note:
-        Descendants of `Simulator` should override `simulate()`.
-    
-    Attributes:
-        _factory (MDPFactory): MDP factory.
+        - Descendants of this class should override 
+          `simulate_on_observation()`.
     """
-    
-    def __init__(self, factory: MDPFactory):
-        self._factory = factory
         
-    def simulate(self, s: State, a: Action) -> tuple[Reward, State]:
+    def simulate_on_state(self, s: State, a: Action) -> tuple[Reward, State]:
         """Simulate given action on given state.
         
         Args:
@@ -28,6 +24,27 @@ class Simulator():
         Returns:
             tuple: Immediate reward and next state.
         """
+        r_features, o_features = self.simulate_on_observation(
+                mdp.factory.create_observation(s.get_repr()), a)
+        
+        return (mdp.factory.create_reward(*r_features),
+                mdp.factory.create_state(
+                        mdp.factory.create_observation(*o_features)
+                                   .get_repr()))
+    
+    def simulate_on_observation(self, o: Observation, a: Action) \
+            -> tuple[tuple, tuple]:
+        """Simulate given action on given observation.
+
+        Args:
+            o (Observation): The current observation.
+            a (Action): The action.
+
+        Returns:
+            tuple: features of next reward and next observation. Features of  
+                observation is `None` if the action is illegal on the current  
+                observation.
+        """
         raise NotImplementedError(f'class {self.__class__} did not override ' 
-                                   'simulate()')
+                                  'simulate_on_observation()')
         
